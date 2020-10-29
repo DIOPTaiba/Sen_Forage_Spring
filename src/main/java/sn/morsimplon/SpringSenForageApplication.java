95c0af67b78df7fcaaa81722b59cd3fed1ff1cc0
DIOPTaiba <mordioptaiba@gmail.com>
2020-10-29 17:15:18 +0000

    Creation prjet Sen Forage avec Spring

---
+++ src/main/java/sn/morsimplon/SpringSenForageApplication.java
@@ -0,0 +1,13 @@
+package sn.morsimplon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSenForageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSenForageApplication.class, args);
+	}
+
+}
